d3f01ba5f6108dae7ebb071ee979e0c519bc3817
Hexel Colorado <hexelcolorado@gmail.com>
2025-12-16 04:11:12 -0600

    Add fixture rail lines for A Train and TEXRail

--- src/special.js
+++ src/special.js
@@ -8,12 +8,14 @@ const ORANGE = '#F68B1F';
 const SILVER = '#C0C0C0';
 
 export const sectionIndex = {
-    silverWest: { refShape: '146593', refIndexes: [0, 1, 2, 3, 4, 5, 6], colors: [SILVER] },
+    texRail: { refShape: '33493', refIndexes: [1, 2, 3, 4, 5, 6], colors: ['#e91c2e']},
+    silverTexRail: { refShape: '146593', refIndexes: [0], colors: ['#e91c2e', SILVER] },
+    silverWest: { refShape: '146593', refIndexes: [1, 2, 3, 4, 5, 6], colors: [SILVER] },
     silverShiloh: { refShape: '146593', refIndexes: [7, 8], colors: [SILVER] },
     treVictoryUnion: { refShape: '146574', refIndexes: [8], colors: ['#002579'] },
     orangeDfwAirport: { refShape: '146517', refIndexes: [0, 1, 2, 3, 4, 5, 6], colors: [ORANGE] },
     blueRowlett: { refShape: '146460', refIndexes: [16, 17, 18, 19, 20, 21], colors: [BLUE] },
-    treWest: { refShape: '146574', refIndexes: [0, 1, 2, 3, 4, 5, 6], colors: ['#002579'] },
+    treWest: { refShape: '146574', refIndexes: [1, 2, 3, 4, 5, 6], colors: ['#002579'] },
     treMedicalMarketVictory: { refShape: '146574', refIndexes: [7], colors: ['#002579'] },
     blueUntDallas: { refShape: '146460', refIndexes: [0, 1, 2, 3, 4, 5, 6], colors: [BLUE] },
     greenBuckner: { refShape: '146508', refIndexes: [0, 1, 2, 3, 4, 5, 6, 7], colors: [GREEN]},
@@ -28,7 +30,10 @@ export const sectionIndex = {
     greenOrangeVictory: { refShape: '146508', refIndexes: [11], colors: [ORANGE, GREEN ]},
     greenOrangeTre: { refShape: '146508', refIndexes: [12], colors: [ORANGE, GREEN ]},
     greenOrangeBachman: { refShape: '146508', refIndexes: [13, 14, 15, 16], colors: [ORANGE, GREEN,  ]},
-    greenNorthCarrollton: { refShape: '146508', refIndexes: [17, 18, 19, 20, 21, 22], colors: [GREEN ]},
+    aTrain: { refShape: '4911-A-OB', refIndexes: [0, 1, 2, 3, 4], colors: ['#a98300']},
+    greenATrain: { refShape: '146508', refIndexes: [22], colors: [GREEN, '#a98300']},
+    greenDowntownCarollton: { refShape: '146508', refIndexes: [17, 18, 19, 20, 21], colors: [GREEN ]},
+    treTexRail: { refShape: '33493', refIndexes: [0], colors: ['#e91c2e', '#002579']},
 };
 
 export function getSpecialShape(key) {
