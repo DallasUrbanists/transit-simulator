133897a48057f108e186e79d8b6720cb279c018d
Hexel Colorado <hexelcolorado@gmail.com>
2025-12-24 17:52:34 -0600

    Add test.html as an entry point

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
-export default {
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   base: process.env.BASE_URL || '/transit-simulator/',
-};
+  build: {
+    rollupOptions: {
+      input: {
+        appMain: resolve(__dirname, 'index.html'),
+        appTest: resolve(__dirname, 'test.html'),
+      },
+    },
+  },
+})
